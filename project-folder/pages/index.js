942f8a83a40c2de91afd908503e836423d71262d
yeonju <0751choi@gmail.com>
2024-12-16 09:57:36 +0900

    Add missing files for Christmas project

---
+++ project-folder/pages/index.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div>
+      <h1>Welcome to the Christmas Page!</h1>
+      <p>Explore the Christmas features and comments section.</p>
+    </div>
+  );
+}
